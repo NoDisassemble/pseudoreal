c2c3a946183bb69dcc8d94d9b2c4f7d9e5e19a19
NoDisassemble <anything@dant35an0nym0u5.com>
2022-07-25 23:24:49 -0700

    Add files via upload

--- form.js
+++ form.js
@@ -1,15 +1,12 @@
 // Form powered by FormSubmit: https://formsubmit.co/
-// 
 // Ajax via fetch info here: https://formsubmit.co/ajax-documentation
 
 const formName = document.getElementById("formName").value;
 const formEmail = document.getElementById("formEmail").value;
 const formMessage = document.getElementById("formMessage").value;
 
-
-function formSubmit() {
-    console.log("button has been clicked");
-    fetch("https://formsubmit.co/ajax/dr.unit@gmail.com", {
+async function formSubmit() {
+    response = await fetch("https://formsubmit.co/ajax/dr.unit@gmail.com", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
@@ -21,9 +18,29 @@ function formSubmit() {
             message: `${formMessage}`,
         })
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+    data = await response.json();
+    console.log(data);
+}
+// Alerts using bootstrap component
+// https://getbootstrap.com/docs/5.2/components/alerts/
 
+const messageSent = document.getElementById('alertSuccess')
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    messageSent.append(wrapper)
 }
 
+const alertTrigger = document.getElementById('submit')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        alert('Message Sent!', 'success')
+    })
+}
