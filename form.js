a058e68cd191c700a1efa8e235b7f2b13e1d7dd3
NoDisassemble <anything@dant35an0nym0u5.com>
2022-07-25 20:07:17 -0700

    Add files via upload

--- form.js
+++ form.js
@@ -2,12 +2,13 @@
 // 
 // Ajax via fetch info here: https://formsubmit.co/ajax-documentation
 
-const formName = document.getElementById("formName");
-const formEmail = document.getElementById("formEmail");
-const formMessage = document.getElementById("formMessage");
+const formName = document.getElementById("formName").value;
+const formEmail = document.getElementById("formEmail").value;
+const formMessage = document.getElementById("formMessage").value;
 
 
 function formSubmit() {
+    console.log("button has been clicked");
     fetch("https://formsubmit.co/ajax/dr.unit@gmail.com", {
         method: "POST",
         headers: {
@@ -15,12 +16,14 @@ function formSubmit() {
             'Accept': 'application/json'
         },
         body: JSON.stringify({
-            name: `${formName.value}`,
-            email: `${formEmail.value}`,
-            message: `${formMessage.value}`,
+            name: `${formName}`,
+            email: `${formEmail}`,
+            message: `${formMessage}`,
         })
     })
         .then(response => response.json())
         .then(data => console.log(data))
         .catch(error => console.log(error));
+
 }
+
